849619e8b7f48315854440c3438419afd44a935f
Steve Eliopoulos <steve.eliopoulos@gmail.com>
2016-09-07 20:26:06 -0700

    update baseball api page

--- jsd2_homepage/final_project/js/final_project.js
+++ jsd2_homepage/final_project/js/final_project.js
@@ -1,106 +1,93 @@
-<script type="text/javascript">
-    $(function() {
-        var params = {
-            // Request parameters
-        };
-      
-        $.ajax({
-            url: "https://api.fantasydata.net/mlb/v2/{format}/teams?" + $.param(params),
-            beforeSend: function(xhrObj){
-                // Request headers
-                xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","5e2a5ce1853e470a81bd6355e3321b4c");
-            },
-            type: "GET",
-            // Request body
-            data: "{body}",
-        })
-        .done(function(data) {
-            alert("success");
-        })
-        .fail(function() {
-            alert("error");
-        });
-    });
-    -->
-     <style>
-      html, body {
-        height: 0%;
-        margin: 0;
-        padding: 10;
-      }
-      .map {
-        height: 100%;
-        width:100%;
-      }
-        </style>
-</script>
-<!-- 
-navigator.geolocation.getCurrentPosition(updateLocation);
- var map;
-
-function updateLocation(positionObject){
-   var lat = position.coords.latitude;
-   var lng = position.coords.longitude;
-
-   var marker = new google.maps.Marker({
-    map: map,
-    position: {
-        lat: lat,
-        lng: lng
-    }
-   })*/
-
- -->
- <script>
- function createMarker(place){
-         var marker = new google.maps.Marker({
-            map: map,
-            title: "GA",
-            position:place.geometry.location
-    });
-var mapElement = document.querySelector('.map');
+// Setup
+// ------------------------------------------
 var options = {
-    center: {lat:37.790841,lng:-122.40128},
-    zoom: 10
-
-};
-function initMap(){
-console.log('initMap')
-
-    var map;
-    map = new google.maps.Map(mapElement, options)
-    console.log('initMap');
-    var marker = new google.maps.Marker({map: map,
-        title: "GA",
-        position:{lat:37.790841,lng:-122.40128}
-    });
-    var service = new google.maps.places.PlacesService(map);
-    service.nearbySearch({
-        location:{lat:37.790841,lng:-122.40128},
-        radius:100,
-        type: ['store']
-    },
-        displayResults);
-
-    function displayResults(results, status){
-        console.log('displayResults');
-
-        debugger
-
-/*        results.forEach(createMarker)
-*/    }
-    function createMarker(place){
-         var marker = new google.maps.Marker({
-            map: map,
-            title: "GA",
-            position:place.geometry.location
-    });
-    }
-
-}
-
-
+    enableHighAccuracy: true,
+    maximumAge: 3000,
+    timeout: 2700
 };
 
-}
-</script>
+var geoMarker;
+
+
+// Structure
+// ------------------------------------------
+var button    = document.querySelector('main button');
+var locations = document.querySelector('main .locations');
+var error     = document.querySelector('main .error');
+var element = document.querySelector('main .map');
+var marker;
+var map;
+// Events
+// ------------------------------------------
+button.addEventListener('click', clickButton);
+
+
+// Event Handlers
+// ------------------------------------------
+if ("geolocation" in navigator) {
+
+function clickButton(initMap) {
+    navigator.geolocation.watchPosition(geoSuccess, geoError, options)
+    };
+
+    function geoError(positionError) {
+    error.innerHTML = 'Error: Unable to retrieve your location. ' +  positionError.code + ': ' + positionError.message;
+};
+};
+
+// Geolocation callback functions
+// ------------------------------------------
+function geoSuccess(position){
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    
+    console.log('geoSuccess',latitude,longitude);
+    updateLocation(latitude, longitude);
+/*  placeMarker(latitude,longitude);
+*/};
+
+
+
+// Update page functions
+// ------------------------------------------
+function updateLocation(latitude, longitude) {
+    console.log('updateLocation',latitude,longitude);
+
+    var li = document.createElement('li');
+    li.innerHTML = latitude + ' , ' + longitude;
+    locations.appendChild(li);
+
+
+    placeMarker(latitude,longitude);
+};
+
+
+
+// Callback when Google Maps has loaded
+// ------------------------------------------
+
+function initMap() {
+    map = new google.maps.Map(element, {
+        center: {lat: 37.790841, lng: -122.40128},
+        zoom: 12
+    });
+};
+
+
+// Add / update the location marker
+// ------------------------------------------
+
+
+function placeMarker(latitude,longitude) {
+    console.log('placeMarker',latitude,longitude);
+
+    if (marker) {
+        marker.setMap(null);
+    };
+    console.log('past if marker!');
+
+    marker = new google.maps.Marker({
+        map:map,
+        position: {lat:latitude, lng:longitude}
+    });
+};
